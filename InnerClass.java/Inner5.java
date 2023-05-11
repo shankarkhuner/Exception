768c97ba7a84ae936a69ba1fa13b69abe1c1ff06
shankarkhuner <shankarkhuner@gmail.com>
2023-05-11 19:15:34 +0530

    Inner class

--- InnerClass.java/Inner5.java
+++ InnerClass.java/Inner5.java
@@ -1,3 +1,19 @@
+/*
+Inner class Member can not be access in the outer class directly. But can be access with the help of object cretion.
+
+
+
+*/
+class Outer
+{
+	int a = 100;
+	void Inner()
+		{
+			System.out.println("Inside the method m1");
+			Inner s1 = new Inner();
+			System.out.println(s1.a);
+		}
+}
 class Demo
 {
 	public static void main(String[] args) 
---
+++ InnerClass.java/Inner6.java
@@ -0,0 +1,22 @@
+
+class Outer
+{int a = 100;
+	float b = 200.5f;
+class Inner
+{int x = 400;
+	float y = 500.54f;
+	void m1()
+		{System.out.println(a+b);
+		System.out.println(x+y);
+		}
+}
+}
+class Demo 
+{public static void main(String[] args) 
+	{System.out.println("Hello World!");
+		//Outer o1 = new Outer();
+		//Outer.Inner i = o1.new Inner();
+		//Outer.Inner i = new Outer().new Inner();
+		new Outer().new Inner().m1();
+	}
+}
