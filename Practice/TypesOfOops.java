238e3b1478d24f9f9ccaa0fbb23eb7cfff82187c
shankarkhuner <shankarkhuner@gmail.com>
2023-05-16 22:36:19 +0530

    New File

---
+++ Practice/TypesOfOops.java
@@ -0,0 +1,28 @@
+
+// using Inheritance we achive optimum code we achive Reuseabelity Time of Excecution is less No repetation occeres 
+
+
+class Sample  
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+	}
+}
+/*Types of OOPS 
+
+1) Inheritance
+	i)Single Inheritance
+	ii)Multilevel Inheritance
+	iii)Hirarchial Inheritance
+	iv)Hultiple Inheritance
+	v)Hybride Inheritance
+
+2) Polymorphism
+3) Abstraction
+4) Encapsulation
+
+
+
+
+*/
